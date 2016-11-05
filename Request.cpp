70c62626c6457f6eb4ff6d63998816de2d2fe9f5
bigYelloDuck <givemeyourfox@gmail.com>
2016-11-05 17:22:14 +0800

    Request

---
+++ Request.cpp
@@ -0,0 +1,63 @@
+#include "Request.h"
+
+#include <algorithm>
+
+namespace urllib
+{
+
+using std::string;
+using std::pair;
+using std::vector;
+
+Request::Request(const string& url)
+    : url_(url)
+{
+    initHeaders();
+}
+
+Request::Request(const char* url)
+    : url_(string(url))
+{
+    initHeaders();
+}
+
+const string& Request::url() const noexcept
+{
+    return url_;
+}
+
+const vector<pair<const string, string>>& Request::headers() const noexcept
+{
+    return headers_;
+}
+
+bool Request::addHeader(const string& key, const string& value)
+{
+    auto iter = std::find_if(headers_.begin(), headers_.end(),
+            [key](pair<const string, string> header)
+            { return header.first == key; });
+
+    if (iter == headers_.end())
+    {
+        pair<const string, string> header(key, value);
+        headers_.push_back(header);
+    }
+    else
+    {
+        iter->second = value;
+    }
+
+    return true;
+}
+
+void Request::initHeaders()
+{
+    headers_.push_back({"Accept", "text/html"});
+    headers_.push_back({"Accept-Encoding", "gzip, deflate, sdch"});
+    headers_.push_back({"Accept-Language", "en-US,en;q=0.5"});
+    headers_.push_back({"Host", url_});
+    headers_.push_back({"User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"});
+}
+
+}  // end of namespace urllib
+
---
+++ Request.h
@@ -0,0 +1,35 @@
+#ifndef URLLIB_REQUEST_H
+#define URLLIB_REQUEST_H
+
+#include <string>
+#include <vector>
+#include <utility>
+
+namespace urllib
+{
+
+class Request
+{
+ public:
+    explicit Request(const std::string& url);
+    Request(const char* url);
+
+    // using defualt copy ctor/assignment, move ctor/assignment, dtor
+ 
+    const std::string& url() const noexcept;
+    const std::vector<std::pair<const std::string, std::string>>& headers() const noexcept;
+
+    bool addHeader(const std::string& key, const std::string& value);
+ private:
+    void initHeaders();
+
+    std::string url_;
+    std::vector<std::pair<const std::string, std::string>> headers_;
+};
+
+
+
+}  // end of namespace urllib
+
+#endif // URLLIB_REQUEST_H
+
