b809c1e7c863de17ede51cec0118180f42d64cd6
bigYelloDuck <givemeyourfox@gmail.com>
2016-11-07 23:37:44 +0800

    test the urllib

---
+++ testurl.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+#include "Request.h"
+#include "Response.h"
+#include "urlutil.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        cout << "usage: testurl <url>" << endl;
+        return 0;
+    }
+
+    const string url(argv[1]);
+    
+    urllib::Request req(url);
+//    req.addHeader("Accept-Language", "zh-CN,zh; q=0.8");
+    auto resp = urllib::urlOpen(req);
+    ofstream fcout("xxx");
+    fcout << resp.read();
+    return 0;
+}
